dda16f1768827863adb338948a4cea7d8f29f456
HwangTaehyun <eeht1717@gmail.com>
2020-04-23 04:35:46 +0900

    [feat] Add @Repeat custom annotation
    - Target을 METHOD로 설정
    - RetetionPolicy를 RUNTIME으로 설정
    - default count값을 1로 설정

---
+++ src/test/java/com/naver/commerce_platform/junit/Repeat.java
@@ -0,0 +1,14 @@
+package com.naver.commerce_platform.junit;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Repeat {
+    int count() default 1;
+}
+
+
