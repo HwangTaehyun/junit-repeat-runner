8ef4671e4f3950ea04b804e1f4ab33de43a6793a
HwangTaehyun <eeht1717@gmail.com>
2020-04-24 02:45:47 +0900

    [feat] 단위 테스트 상황에 맞는 로그 메시지 출력
    - JUnit의 TestWatcher를 이용해 단위 테스트 상황에 맞는 로그 메시지 출력
      - @Override: succeeded, failed, starting, finished

--- src/test/java/com/naver/commerce_platform/junit/CalculatorTest.java
+++ src/test/java/com/naver/commerce_platform/junit/CalculatorTest.java
@@ -1,7 +1,12 @@
 package com.naver.commerce_platform.junit;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.junit.runners.model.Statement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,12 +19,40 @@ public class CalculatorTest {
     static final Logger logger =
             LoggerFactory.getLogger(RepeatRunner.class);
 
+    @Rule
+    public final TestRule watchman = new TestWatcher() {
+        @Override
+        public Statement apply(Statement base, Description description) {
+            return super.apply(base, description);
+        }
+
+        @Override
+        protected void succeeded(Description description) {
+            logger.info("{} unit test succeeded.", description.getMethodName());
+        }
+
+        @Override
+        protected void failed(Throwable e, Description description) {
+            logger.error("{} unit test failed with {}.", description.getMethodName(), e);
+        }
+
+        @Override
+        protected void starting(Description description) {
+            super.starting(description);
+            logger.info("Run {}...", description);
+        }
+
+        @Override
+        protected void finished(Description description) {
+            super.finished(description);
+            logger.info("End {}...", description);
+        }
+    };
+
+
     @Test
     @Repeat(count = 5)
     public void testMyCode5Times() {
-        //Log
-        logger.info("Run testMyCode5Times...");
-
         //Arrange
         Calculator calculator = new Calculator();
         int x = 3;
@@ -35,9 +68,6 @@ public class CalculatorTest {
     @Test
     @Repeat(count = 10)
     public void testMyCode10Times() {
-        //Log
-        logger.info("Run testMyCode5Times...");
-
         //Arrange
         Calculator calculator = new Calculator();
         int x = 5;
