6a414bd50256a5ce2578be13c06d18615ac0e332
HwangTaehyun <eeht1717@gmail.com>
2020-04-25 04:22:53 +0900

    [feat] 단위 테스트별 반복 결과 요약 로그 작성 구현
    - 단위 테스트별 결과 요약 로그를 위해 HashMap 이용
    - 반복 결과(totalCount, passCount, failCount) 구조체 작성

---
+++ src/test/java/com/naver/commerce_platform/junit/RepeatTestInfo.java
@@ -0,0 +1,7 @@
+package com.naver.commerce_platform.junit;
+
+public class RepeatTestInfo {
+    public int totalCount = 0;
+    public int passCount = 0;
+    public int failCount = 0;
+}
--- src/test/java/com/naver/commerce_platform/junit/RepeatTestWatcher.java
+++ src/test/java/com/naver/commerce_platform/junit/RepeatTestWatcher.java
@@ -1,15 +1,28 @@
 package com.naver.commerce_platform.junit;
 
-import jdk.nashorn.internal.objects.annotations.Getter;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RepeatTestWatcher extends TestWatcher {
     static final Logger logger =
             LoggerFactory.getLogger(CalculatorTest.class);
+    static Map<String, RepeatTestInfo> testResult = new HashMap<>();
+
+    public int getTotalCount(String methodName){
+        return this.testResult.get(methodName).totalCount;
+    }
+    public int getPassCount(String methodName){
+        return this.testResult.get(methodName).passCount;
+    }
+    public int getFailCount(String methodName){
+        return this.testResult.get(methodName).failCount;
+    }
 
     @Override
     public Statement apply(Statement base, Description description) {
@@ -19,11 +32,21 @@ public class RepeatTestWatcher extends TestWatcher {
     @Override
     protected void succeeded(Description description) {
         logger.info("{} unit test succeeded.", description.getMethodName());
+        RepeatTestInfo testInfo = testResult.get(description.getMethodName());
+        if (testInfo == null) {
+            testResult.put(description.getMethodName(),new RepeatTestInfo());
+        }
+        testResult.get(description.getMethodName()).passCount++;
     }
 
     @Override
     protected void failed(Throwable e, Description description) {
         logger.error("{} unit test failed with {}.", description.getMethodName(), e);
+        RepeatTestInfo testInfo = testResult.get(description.getMethodName());
+        if (testInfo == null) {
+            testResult.put(description.getMethodName(),new RepeatTestInfo());
+        }
+        testResult.get(description.getMethodName()).failCount++;
     }
 
     @Override
@@ -36,5 +59,10 @@ public class RepeatTestWatcher extends TestWatcher {
     protected void finished(Description description) {
         super.finished(description);
         logger.info("{} unit test finished...", description);
+        RepeatTestInfo testInfo = testResult.get(description.getMethodName());
+        if (testInfo == null) {
+            testResult.put(description.getMethodName(),new RepeatTestInfo());
+        }
+        testResult.get(description.getMethodName()).totalCount++;
     }
 }
